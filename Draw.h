65abe5edbe26b86b149db3ec34341e82651a4d63
joeyst <joeystenbeck@gmail.com>
2023-12-12 18:58:22 -0800

    Move Draw and InitDL functions to Draw.h, organize.

--- Draw.h
+++ Draw.h
@@ -1,26 +1,28 @@
 
-/* Joey's globals, function decls., defines, includes. */
-#include "NetGraphics.h"
+#ifndef _DRAW_H
+#define _DRAW_H
 
 #include "includes.h"
 
-
 GLuint neuron;
+GLuint synapse;
+GLuint box;
+
+void DrawNeuron(vector<int> xyz, vector<Proportion> rgba);
+void DrawSynapse(vector<int> start, vector<int> end, vector<Proportion> rgba);
+void DrawBox(vector<int> center, int r, vector<Proportion> rgba);
+
 void InitNeuronDL();
+void InitSynapseDL();
+void InitBoxDL();
+
 void DrawNeuronf(vector<float> xyz, enum Colors color);
 void DrawNeuronf(vector<float> xyz, enum Colors color, Proportion alpha);
 vector<vector<int>> NeuronCoordsList;
 void DrawNeuron(vector<int> xyz, enum Colors color, Proportion alpha);
-void DrawNeuron(vector<int> xyz, vector<Proportion> rgba);
 
-GLuint synapse;
-void InitSynapseDL();
-void DrawSynapse(vector<int> start, vector<int> end, vector<Proportion> rgba);
 vector<float> GetSynapseDXYZ(vector<int> start, vector<int> end);
 float GetSynapseLength(vector<int> start, vector<int> end);
 
-GLuint box;
-void InitBoxDL();
-void DrawBox(vector<int> center, int r, vector<Proportion> rgba);
 
-/* End of Joey's globals, function decls., defines, includes. */
+#endif
